88c17fc1a39574075a766d61d3178e96b9b082f3
Marcos Quintero <a00824603@itesm.mx>
2021-05-31 00:19:27 -0600

    tablas y reglas 🥶

---
+++ sql/reglas.sql
@@ -0,0 +1,40 @@
+-- ponerle buenos nombres a las reglas
+
+-- regla1
+CREATE RULE regla_1 
+AS ON UPDATE TO Area
+WHERE new.name NOT IN (
+    SELECT d.works
+    FROM Doctor AS d
+    WHERE new.ledBy = d.pId
+)
+DO NOTHING; -- o que imprima un mensaje, no se
+
+-- regla2 
+CREATE RULE regla_2
+AS ON UPDATE TO Doctor
+WHERE new.yearsExperience > old.yearsExperience and new.yearsExperience > 0 AND (new.yearsExperience % 2 = 0)
+DO UPDATE Doctor
+SET salary = old.salary * 1.10
+WHERE pId = old.pId;
+
+
+-- regla3
+
+CREATE RULE regla_3
+AS ON UPDATE TO Doctor
+WHERE old.works != new.works and old.works IN (
+    SELECT name
+    FROM Area
+    WHERE ledBy = new.pId
+)
+DO UPDATE Area
+SET ledBy = (
+    SELECT pId
+    FROM Doctor AS d
+    WHERE d.works = old.works
+    LIMIT 1
+)
+WHERE ledBy = new.pId
+
+
---
+++ sql/tablas.sql
@@ -0,0 +1,51 @@
+-- CREATE DATABASE medicpatient;
+
+-- Primero creamos los tipos necesarios para los planes y para especialidades
+CREATE TYPE insurancePlan_t AS ENUM('Unlimited', 'Premium', 'Basic');
+CREATE TYPE speciality_t AS ENUM('General Medicine', 'Traumatology', 'Allergology', 'Radiology', 'Cardiology', 'Gerontology', 'Obstetrics', 'Pediatrics');
+
+-- Las tablas
+
+CREATE TABLE Person (
+    pId       int,
+    firstName varchar(20),
+    lastName  varchar(20),
+    dob       date,
+    gender    varchar(1),
+
+    PRIMARY key (pId)
+);
+
+-- las tablas doctor y patient heredan de person
+
+CREATE TABLE Patient (
+    pId           int,
+    insurancePlan insurancePlan_t,
+    PRIMARY KEY (pId)
+) INHERITS (Person);
+
+create table Doctor (
+    pId             int, 
+    specialities    speciality_t[],
+    yearsExperience int,
+    salary          float,
+    PRIMARY KEY (pId)
+);
+
+CREATE TABLE Area (
+    name     speciality_t,
+    location varchar(30),
+    ledBy    int REFERENCES Doctor(pId),
+    PRIMARY KEY (name)
+);
+
+-- necesita ir despues del create table de Area
+ALTER TABLE Doctor ADD COLUMN works speciality_t REFERENCES Area(name);
+
+CREATE TABLE Treatment (
+    receivedBy   int REFERENCES Patient(pId),
+    prescribedBy int REFERENCES Doctor(pId),
+    duration     int,
+    medicaments  varchar(25)[],
+    description  varchar(80)
+)
